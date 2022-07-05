efdddc22eb1703558a9c3e79ea430a886af88ae7
Amit Kumar <83487500+amitkrxyz@users.noreply.github.com>
2022-07-05 12:58:43 +0530

    refactor and bug fixes

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
+const body = document.querySelector("body");
 const firstNumText = document.querySelector("#first-num");
 const operatorText = document.querySelector("#operator");
 const secondNumText = document.querySelector("#second-num");
 const btns = document.querySelectorAll(".btn");
-const incrLevel = document.querySelector("#increment");
-const decrLevel = document.querySelector("#decrement");
+const incrLevelBtn = document.querySelector("#increment");
+const decrLevelBtn = document.querySelector("#decrement");
 const scoreText = document.querySelector("#score");
 const hiscoreText = document.querySelector("#hiscore");
 const levelText = document.querySelector("#level");
 let hiscore = 0;
 let score = 0;
 let level = 10;
-
-const setEverything = () => {
-  const firstNum = Math.floor(Math.random() * level);
+const operators = ["+", "-", "x"];
+let correctAns = setQuestion();
+initialize();
+function initialize() {
+  btns.forEach((btn) => {
+    btn.addEventListener("click", btnClicked);
+  });
+  incrLevelBtn.addEventListener("click", incrLevel);
+  decrLevelBtn.addEventListener("click", decrLevel);
+}
+function incrLevel() {
+  const currLevel = parseInt(levelText.textContent);
+  if (currLevel < 3) {
+    level = level * 10;
+    levelText.textContent = currLevel + 1;
+    restart();
+  }
+}
+function decrLevel() {
+  const currLevel = parseInt(levelText.textContent);
+  if (currLevel > 1) {
+    level = Math.floor(level / 10);
+    levelText.textContent = currLevel - 1;
+    restart();
+  }
+}
+function setQuestion() {
+  let firstNum = Math.floor(Math.random() * level);
   let secondNum = Math.floor(Math.random() * level);
-  const operators = ["+", "-", "x"];
   const operator = operators[Math.floor(Math.random() * operators.length)];
   const wrongAnswers = new Set();
   let correctAns = null;
@@ -38,16 +63,36 @@ const setEverything = () => {
       }
     }
   } else if (operator === "x") {
-    correctAns = firstNum * secondNum;
+    if (level > 100) {
+      firstNum = Math.floor(Math.random() * 100);
+      secondNum = Math.floor(Math.random() * 100);
+      while (wrongAnswers.size < 4) {
+        const wrongAns =
+          Math.floor(Math.random() * 21) * Math.floor(Math.random() * 10);
+        if (wrongAns !== correctAns) {
+          wrongAnswers.add(wrongAns);
+        }
+      }
+    } else if (level > 10) {
+      firstNum = Math.floor(Math.random() * 21);
+      secondNum = Math.floor(Math.random() * 10);
+      while (wrongAnswers.size < 4) {
+        const wrongAns =
+          Math.floor(Math.random() * 21) * Math.floor(Math.random() * 10);
+        if (wrongAns !== correctAns) {
+          wrongAnswers.add(wrongAns);
+        }
+      }
+    }
     while (wrongAnswers.size < 4) {
       const wrongAns =
-        Math.floor(Math.random() * level) * Math.floor(Math.random() * level);
+        Math.floor(Math.random() * 21) * Math.floor(Math.random() * 10);
       if (wrongAns !== correctAns) {
         wrongAnswers.add(wrongAns);
       }
     }
+    correctAns = firstNum * secondNum;
   }
-  console.log(secondNum);
   operatorText.textContent = operator.toString();
   firstNumText.textContent = firstNum.toString();
   secondNumText.textContent = secondNum.toString();
@@ -62,39 +107,32 @@ const setEverything = () => {
     }
   }
   return correctAns;
-};
+}
+function btnClicked() {
+  checkAnswer(this);
+  update();
+}
 
-let correctAns = setEverything();
-btns.forEach((element) => {
-  element.addEventListener("click", () => {
-    if (element.textContent == correctAns) {
-      const currLevel = parseInt(levelText.textContent);
-      score += currLevel;
-      if (score > hiscore) {
-        hiscore = score;
-      }
-    } else {
-      score = 0;
+function checkAnswer(btn) {
+  if (btn.textContent == correctAns) {
+    const currLevel = parseInt(levelText.textContent);
+    score += currLevel;
+    if (score > hiscore) {
+      hiscore = score;
     }
-    scoreText.textContent = score;
-    hiscoreText.textContent = hiscore;
-    correctAns = setEverything();
-  });
-});
+  } else {
+    score = 0;
+  }
+}
 
-incrLevel.addEventListener("click", () => {
-  const currLevel = parseInt(levelText.textContent);
-  if (currLevel < 3) {
-    level = level * 10;
-    correctAns = setEverything();
-    levelText.textContent = currLevel + 1;
-  }
-});
-decrLevel.addEventListener("click", () => {
-  const currLevel = parseInt(levelText.textContent);
-  if (currLevel > 1) {
-    level = Math.floor(level / 10);
-    correctAns = setEverything();
-    levelText.textContent = currLevel - 1;
-  }
-});
+function update() {
+  scoreText.textContent = score;
+  hiscoreText.textContent = hiscore;
+  correctAns = setQuestion();
+}
+
+function restart() {
+  hiscore = 0;
+  score = 0;
+  update();
+}
